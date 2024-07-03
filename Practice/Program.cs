91be488027855f606d413add3a4a882f7d5c3000
koffela <46707662+koffela@users.noreply.github.com>
2024-07-03 08:17:07 -0700

    Initial commit

---
+++ Practice/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+//char character = 'c';
+
+//string name = "Aaron";
+
+//this is a comment
+/*this is also a comment*/
+
+//bool on = true;
+
+//int x = 5;
+
+/*
+for(int i = 0; i < 2; i++)
+{
+    Console.WriteLine(x);
+    x++;
+}
+*/
+/*
+while(on == true)
+{
+    Console.WriteLine("The switch is on!");
+}
+*/
+/*
+if(on == false)
+{
+    Console.WriteLine("The switch is off!");
+}
+else
+{
+    Console.WriteLine("The switch is on!");
+}
+*/
+/*
+switch(x)
+{
+    case 0: Console.WriteLine("x is " + x);
+    break;
+    case 5: Console.WriteLine("x is " + x);
+    break;
+    default: Console.WriteLine("we do not know what x is.");
+    break;
+}
+*/
---
+++ Practice/README.md
@@ -0,0 +1,2 @@
+# Practice
+ A practice program that I will use to teach people to program!
